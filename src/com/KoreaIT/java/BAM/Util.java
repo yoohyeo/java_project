ed6de0fa117679ed0317836ea4bd22e3afa4a440
yoohyeon <ghfoqwo3702@gmail.com>
2022-12-03 14:07:14 +0900

    중복 함수로 빼서 표현하기및 테스트데이터 생성

---
+++ src/com/KoreaIT/java/BAM/Util.java
@@ -0,0 +1,12 @@
+package com.KoreaIT.java.BAM;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+	/** 현재 날짜와 시간 String*/
+	public static String getTimeAndDateStr() {
+		LocalDateTime now = LocalDateTime.now();
+		return now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+}
